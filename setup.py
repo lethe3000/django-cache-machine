f947f374affc36c587d246d9740bac60c14c5564
lethe <lethe30003000@gmail.com>
2013-06-26 10:09:30 +0800

    remove classifier

--- setup.py
+++ setup.py
@@ -16,16 +16,4 @@ setup(
     packages=['caching', 'caching.backends'],
     include_package_data=True,
     zip_safe=False,
-    classifiers=[
-        'Development Status :: 1 - Beta',
-        'Environment :: Web Environment',
-        # I don't know what exactly this means, but why not?
-        'Environment :: Web Environment :: Zoom',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
 )
