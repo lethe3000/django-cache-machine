9aab19f660623efb9dfbebcd369d673efcdea248
lethe <lethe30003000@gmail.com>
2013-06-26 10:07:30 +0800

    fix some setup info

--- setup.py
+++ setup.py
@@ -9,18 +9,18 @@ setup(
     description='Automatic caching and invalidation for Django models '
                 'through the ORM.',
     long_description=open('README.rst').read(),
-    author='Jeff Balogh',
-    author_email='jbalogh@mozilla.com',
-    url='http://github.com/jbalogh/django-cache-machine',
+    author='Lethe',
+    author_email='lethe30003000@mozilla.com',
+    url='https://github.com/lethe3000/django-cache-zoom',
     license='BSD',
     packages=['caching', 'caching.backends'],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 1 - Beta',
         'Environment :: Web Environment',
         # I don't know what exactly this means, but why not?
-        'Environment :: Web Environment :: Mozilla',
+        'Environment :: Web Environment :: Zoom',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
